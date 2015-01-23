4045e3fe7d8af60da6db7b1cdf8ad326aa7f5c7c
William Lawson <lawson.bj@gmail.com>
2015-01-23 09:57:00 -0500

    No DB Default

--- lib/schema_plus.rb
+++ lib/schema_plus.rb
@@ -2,7 +2,6 @@ require 'active_record'
 require 'valuable'
 
 require 'schema_plus/version'
-require 'schema_plus/active_record/attribute'
 require 'schema_plus/active_record/base'
 require 'schema_plus/active_record/column_options_handler'
 require 'schema_plus/active_record/db_default'
@@ -153,7 +152,6 @@ module SchemaPlus
     ::ActiveRecord::Schema.send(:include, SchemaPlus::ActiveRecord::Schema)
     ::ActiveRecord::SchemaDumper.send(:include, SchemaPlus::ActiveRecord::SchemaDumper)
     ::ActiveRecord.const_set(:DB_DEFAULT, SchemaPlus::ActiveRecord::DB_DEFAULT)
-    ::ActiveRecord::Attribute.send(:include, SchemaPlus::ActiveRecord::Attribute) if defined? ::ActiveRecord::Attribute
   end
 
 end
